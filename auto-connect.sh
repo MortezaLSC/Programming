542fbd6fda783cdba064e63a98819a3afb3b15ef
MortezaLSC <mrtzlsc@gmail.com>
2014-01-24 13:33:42 +0330

    Update auto-connect.sh

--- auto-connect.sh
+++ auto-connect.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #This script is written by "MortezaLSC" on Friday, 24 January 2014 at 1:00 PM
+#First of all it creates files for each server. After that it connects to all of them automatically and copy file2 from
+#them to file1 on my machine.
 
 while read file1 pass port user host file2; do
     touch /tmp/$file1 
