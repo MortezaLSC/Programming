9419bd40e9b4f290d67fadc33cec1f02a9175fbc
MortezaLSC <mrtzlsc@gmail.com>
2014-01-25 19:10:54 +0330

    Update ssl-conf.sh

--- ssl-conf.sh
+++ ssl-conf.sh
@@ -25,20 +25,33 @@ HTTPPATH="/etc/httpd/conf/httpd.conf"
 yum update -y
 yum install httpd curl php mod_ssl openssl -y
 
-openssl genrsa -out ca.key 1024
-openssl req -new -key ca.key -out ca.csr
-openssl x509 -req -days 365 -in ca.csr -signkey ca.key -out ca.crt
-cp ca.crt /etc/pki/tls/certs
-cp ca.key /etc/pki/tls/private/ca.key
-cp ca.csr /etc/pki/tls/private/ca.csr
+openssl genrsa -out ca.key 1024 &
+openssl req -new -key ca.key -out ca.csr &
+openssl x509 -req -days 365 -in ca.csr -signkey ca.key -out ca.crt &
+cp ca.crt /etc/pki/tls/certs &
+cp ca.key /etc/pki/tls/private/ca.key &
+cp ca.csr /etc/pki/tls/private/ca.csr &
 
 echo ServerName localhost >> $HTTPPATH
 
-sed -i 's|SSLCertificateFile /etc/pki/tls/certs/localhost.crt|SSLCertificateFile /etc/pki/tls/certs/ca.crt|' $SSLPATH
-sed -i 's|SSLCertificateKeyFile /etc/pki/tls/private/localhost.key|SSLCertificateKeyFile /etc/pki/tls/private/ca.key|' $SSLPATH
+sed -i 's|SSLCertificateFile /etc/pki/tls/certs/localhost.crt|SSLCertificateFile /etc/pki/tls/certs/ca.crt|' $SSLPATH &
+sed -i 's|SSLCertificateKeyFile /etc/pki/tls/private/localhost.key|SSLCertificateKeyFile /etc/pki/tls/private/ca.key|' $SSLPATH &
 
 /etc/init.d/iptables stop &
-echo IPTABLES was stopped successfully
+if [ -f /etc/sysconfig/iptables ] && [ $? -eq 0 ]; then
+    echo "iptables stopped successfully...  :)"
+
+elif [ ! -f /etc/sysconfig/iptables ]; then
+    echo "iptables service not found...  :|"
+
+elif [ -f /etc/sysconfig/iptables ] && [ $? -ne 0 ]; then
+    echo "iptables stopping failed...  :("
+fi
+
 
 /etc/init.d/httpd restart &
-echo HTTPD was restarted successfully
+if [ $? -eq 0 ]; then
+    echo "httpd was restarted successfully...  :)"
+else
+    echo "httpd restarting failed...  :("
+fi
