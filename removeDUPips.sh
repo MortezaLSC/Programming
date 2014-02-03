1514754f3654873170db9c281c0cd0730eac4aa0
MortezaLSC <mrtzlsc@gmail.com>
2014-02-03 10:33:45 +0330

    Update removeDUPips.sh

--- removeDUPips.sh
+++ removeDUPips.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 #This script is written by "MortezaLSC" on Saturday, 11 January 2014 at 8:20 AM
-#If you want to remove duplicated IPs from your file using sed command just run this script
+#If you want remove duplicated IPs from your file using sed command just run on of the following solutions in this script
 
-cat FILE1.txt | while read IP ; do sed -i "/^${IP}$/d" FILE2.txt ; done
+##sed
+#cat file.txt | while read IP ; do sed -i "/^${IP}$/d" duplicated.txt ; done
+
+##uniq and sort
+#sort -u file.txt > out.txt
+
+##uniq without sorting
+#uniq file.txt > out.txt
+
+##awk
+#awk '!a[$0]++' input.txt > out.txt
